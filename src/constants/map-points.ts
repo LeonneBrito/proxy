b362e55a3e816a0a1859b49381234acc74fa2fbc
LeonneBrito <britoleonne@gmail.com>
2025-07-09 22:04:43 -0300

    fix: update mapPoints to remove outdated entries and retain relevant data

--- src/constants/map-points.ts
+++ src/constants/map-points.ts
@@ -1,5 +1,5 @@
 export const mapPoints: {
   [key: string]: { label: string; lat: number; lng: number }[]
 } = {
-  n401mdq8ytb: [{ label: 'Entrega?', lat: 796.75, lng: 907.375 }],
+  ja4nifrjmwh: [{ label: 'Entrega?', lat: 1617, lng: 825.125 }],
 }
