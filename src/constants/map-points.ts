3d5748c403be0662b719d4aea75ae0deb036d616
Leonne Brito <britoleonne@gmail.com>
2025-07-12 19:40:00 -0300

    fix: update mapPoints to correct entries for delivery locations

--- src/constants/map-points.ts
+++ src/constants/map-points.ts
@@ -1,5 +1,6 @@
 export const mapPoints: {
   [key: string]: { label: string; lat: number; lng: number }[]
 } = {
-  ja4nifrjmwh: [{ label: 'Entrega?', lat: 1617, lng: 825.125 }],
+  '93dl32utovj': [{ label: 'Entrega?', lat: 465.5, lng: 636.25 }],
+  rhp4lw9m7yg: [{ label: 'Entrega?', lat: 714.25, lng: 953.25 }],
 }
