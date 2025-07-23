98f26b60b756e23b5d0edd68c3b84788814cb6ca
Leonne Brito <leonne.brito@semantix.ai>
2025-07-22 21:01:48 -0300

    fix: update mapPoints to remove outdated entries and restore accurate delivery locations

--- src/constants/map-points.ts
+++ src/constants/map-points.ts
@@ -1,8 +1,5 @@
 export const mapPoints: {
   [key: string]: { label: string; lat: number; lng: number }[]
 } = {
-  '75am2i7xt8q': [
-    { label: 'Entrega?', lat: 378.61363983154297, lng: 1152.772705078125 },
-  ],
-  k604u03fc5a: [{ label: 'Entrega?', lat: 768.5, lng: 1041.75 }],
+  xuk06pdhin: [{ label: 'Entrega?', lat: 714.25, lng: 912.5 }],
 }
