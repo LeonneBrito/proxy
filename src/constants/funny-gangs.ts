b7676f358cc7dafe24bacb7e4e8decffba022f51
Leonne Brito <leonne.brito@semantix.ai>
2025-09-09 21:21:05 -0300

    fix: remove outdated gang names from funnyGangs constant

--- src/constants/funny-gangs.ts
+++ src/constants/funny-gangs.ts
@@ -1,17 +1,14 @@
 export const funnyGangs: { [key: string]: string } = {
   angels: 'Pombas da Paz',
   insanos: 'Doidões S/A',
-  infamous: 'InFamosinhos',
   ballas: 'Boladas',
-  hustlers: 'Hustleiros',
   blackrose: 'Flor Trevosa',
   cesarini: 'Césarzinho',
   families: 'Fámílias Reunidas',
-  sharksix: 'Tralalero Tralala',
   mib: 'Homens de Preto e Azul',
-  anarquia: 'Anarquinhos',
   cyberdystopia: 'Ciber Bagunça',
   thelost: 'Perdidos e Achados',
   vagos: 'Taxistas',
   bandoleros: 'Restaurante Mexicano',
+  strapz: 'Alcinhas',
 }
